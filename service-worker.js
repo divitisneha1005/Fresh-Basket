452f0903b130815450bd8122b5172b4cf6224a78
divitisneha1005 <divitisneha2006@gmail.com>
2026-08-23 19:50:49 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,66 @@
+const CACHE_NAME = "fresh-basket-v1";
+
+const FILES = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./script.js",
+    "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+
+    event.waitUntil(
+
+        caches.open(CACHE_NAME)
+            .then(cache => {
+
+                return cache.addAll(FILES);
+
+            })
+
+    );
+
+});
+
+
+self.addEventListener("fetch", event => {
+
+    event.respondWith(
+
+        caches.match(event.request)
+            .then(response => {
+
+                return response ||
+                       fetch(event.request);
+
+            })
+
+    );
+
+});
+
+
+self.addEventListener("activate", event => {
+
+    event.waitUntil(
+
+        caches.keys().then(keys => {
+
+            return Promise.all(
+
+                keys
+                    .filter(key =>
+                        key !== CACHE_NAME
+                    )
+                    .map(key =>
+                        caches.delete(key)
+                    )
+
+            );
+
+        })
+
+    );
+
+});
